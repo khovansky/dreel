85e3c548c311489d5f7a56ba0b2bd67e25726f38
Yakov <yakovkhovansky@mail.ru>
2018-04-05 11:13:52 +0700

    11

--- example/src/pages/tab2/tab2.ts
+++ example/src/pages/tab2/tab2.ts
@@ -2,15 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import {Tab2DetailPage} from "../tab2-detail/tab2-detail";
 
-
 import { Projects } from '../../providers/providers';
 import { Project } from '../../models/project';
-/**
- * Generated class for the Tab2Page page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -18,15 +11,12 @@ import { Project } from '../../models/project';
   templateUrl: 'tab2.html',
 })
 export class Tab2Page {
-
   currentItems : Project[]=[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public projects: Projects, public modalCtrl: ModalController) {
   }
 
   ionViewDidLoad() {
-
-
     this.projects.getItems().subscribe((resp: any) => {
       console.log(resp);
       // this.navCtrl.push(MainPage);
