d943c1c845d0e429d7eaefd4f6eec0bb2fac5464
Yakov <yakovkhovansky@mail.ru>
2018-05-23 23:09:08 +0700

    111

--- example/src/pages/tab3/tab3.ts
+++ example/src/pages/tab3/tab3.ts
@@ -3,61 +3,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import * as moment from 'moment';
 import { interval } from 'rxjs/observable/interval';
 import { Subscription } from 'rxjs/Subscription';
-/**
- * Generated class for the Tab3Page page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {Subject} from "rxjs/Subject";
+import {takeUntil} from "rxjs/operators";
 
 @IonicPage()
 @Component({
   selector: 'page-tab3',
   templateUrl: 'tab3.html',
 })
-export class Tab3Page implements OnInit, OnDestroy {
+export class Tab3Page{
   public currentTime;
   public timeFormatted;
   public interval;
-  public intervalSubscrition: Subscription;
+  public i = 0;
+  public stopInterval: Subject<any>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.currentTime = moment().utcOffset(0);
     this.currentTime.set({hour:0,minute:0,second:0,millisecond:0});
     this.currentTime.toISOString();
     this.currentTime.format();
+    this.stopInterval = new Subject<any>();
     console.log(this.currentTime.format());
-  }
-
-  ngOnInit() {
-
-  }
-
-  ngOnDestroy() {
-    if (this.intervalSubscrition) {
-      this.intervalSubscrition.unsubscribe();
-    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Tab3Page');
   }
-  test() {
-
-  }
 
   start() {
     this.interval = interval(1000);
 
-    this.intervalSubscrition = this.interval.subscribe(() => {
+    this.interval.pipe(takeUntil(this.stopInterval)).subscribe(() => {
       this.currentTime.add(1, 'seconds');
       this.timeFormatted = this.currentTime.format("mm:ss");
-      console.log(this.timeFormatted);
     });
+
   }
 
   stop() {
-    this.interval.unsubscribe();
+    this.stopInterval.next();
   }
 
 }
