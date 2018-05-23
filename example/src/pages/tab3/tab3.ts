dbe1a5199755cc1dc66fd7bca769048c71de944b
Yakov <yakovkhovansky@mail.ru>
2018-05-23 23:09:42 +0700

    11

--- example/src/pages/tab3/tab3.ts
+++ example/src/pages/tab3/tab3.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import * as moment from 'moment';
 import { interval } from 'rxjs/observable/interval';
-import { Subscription } from 'rxjs/Subscription';
 import {Subject} from "rxjs/Subject";
 import {takeUntil} from "rxjs/operators";
 
