fad5ce15441c587c6bf733686efc959de4cdc3f3
Yakov <yakovkhovansky@mail.ru>
2018-05-24 22:57:39 +0700

    11

--- example/src/pages/eattime-report-list/eattime-report-list.ts
+++ example/src/pages/eattime-report-list/eattime-report-list.ts
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EattimeReportListPage {
 
+  os: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
